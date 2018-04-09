f95fb626d6d126c22a4a50d734c1129511c5d3a0
Kevin Zidi <kevin.zidi@viacesi.fr>
2018-04-09 17:22:55 +0200

    Ajout dossier Functions

---
+++ functions/functions.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KEVIN
+ * Date: 09/04/2018
+ * Time: 17:10
+ */
+
+function aireSphere($rayon) {
+    if ($rayon >= 0) {
+        return $rayon ** 2 * pi() * 4;
+    }
+
+    else {
+        return false;
+    }
+}
+
+function volumeSphere ($rayon) {
+    if ($rayon >= 0) {
+        return 4/3 * pi() * $rayon ** 3;
+    }
+
+    else {
+        return false;
+    }
+}
